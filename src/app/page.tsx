3bb801cf75c0460e5698f62f2a370607eb13cc44
Manoj Nayak <manoj@manojnayak.com>
2025-08-22 06:12:38 +0000

    I am seeing this big space after the footer in mobile chrome

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import Footer from '@/components/layout/footer';
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-dvh">
+    <div className="flex flex-col min-h-screen">
       <Header />
       <main className="flex-1">
         <Hero />
