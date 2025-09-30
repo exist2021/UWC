d74df72118f4bf0b2e75fc0709aa875ce8683d04
Manoj Nayak <manoj@manojnayak.com>
2025-09-30 13:20:57 +0000

    I got this

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,13 +7,13 @@ import { getAnalytics } from 'firebase/analytics';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBG92W0F3lESrRwdLHW-d2tmjGFDcpKMSo",
-  authDomain: "studio-3132237263-6c506.firebaseapp.com",
-  projectId: "studio-3132237263-6c506",
-  storageBucket: "studio-3132237263-6c506.appspot.com",
-  messagingSenderId: "149882375215",
-  appId: "1:149882375215:web:afbf61974fd48ac8fdcada",
-  measurementId: "G-K3PN27KCLL"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 
